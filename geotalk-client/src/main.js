541146f664997a76dc30e3680d3ddd4ad1aca8eb
trm109 <trm109@case.edu>
2022-12-04 17:45:59 -0500

    adding vuetify

--- geotalk-client/src/main.js
+++ geotalk-client/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
 
+Vue.use(Vuetify)
 createApp(App).use(router).mount('#app')
